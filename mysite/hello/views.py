81fb820d064af918cca3187bf3da27e9866e1092
ignacio-nava <ignan_90@hotmail.com>
2021-03-04 17:40:28 -0300

    First Commit

---
+++ mysite/hello/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+import logging
+
+logger = logging.getLogger(__name__)
+
+# Create your views here.
+
+def helloworld(request):
+    logging.error('Hello world DJ4E in the log...')
+    print('Hello world DJ4E in a print statement...')
+    response = """<html><body><p>Hello world DJ4E in HTML</p>
+    <p>This sample code is available at
+    <a href="https://github.com/csev/dj4e-samples">
+    https://github.com/csev/dj4e-samples</a></p>
+    </body></html>"""
+    return HttpResponse(response)
+
+def sessfun(request):
+    num_visits = request.session.get('num_visits', 0) + 1
+    request.session['num_visits'] = num_visits
+    if num_visits > 4 : del (request.session['num_visits'])
+    resp = HttpResponse('view count='+str(num_visits)+' | 709f5f3e ')
+    resp.set_cookie('dj4e_cookie', '709f5f3e', max_age=1000)
+    return resp
